aff8c6d8927169a91307c77f4f27ebfac2566c92
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2021-07-19 18:07:39 +0530

    Update misc.py

--- cinderella/modules/helper_funcs/misc.py
+++ cinderella/modules/helper_funcs/misc.py
@@ -42,44 +42,44 @@ def split_message(msg: str) -> List[str]:
 def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if not chat:
         modules = sorted(
-            [EqInlineKeyboardButton(x.__mod_name__,
-                                    callback_data="{}_module({})".format(prefix, x.__mod_name__.lower())) for x
-             in module_dict.values()])
+            [
+                EqInlineKeyboardButton(
+                    x.__mod_name__,
+                    callback_data="{}_module({})".format(
+                        prefix, x.__mod_name__.lower()
+                    ),
+                )
+                for x in module_dict.values()
+            ]
+        )
     else:
         modules = sorted(
-            [EqInlineKeyboardButton(x.__mod_name__,
-                                    callback_data="{}_module({},{})".format(prefix, chat, x.__mod_name__.lower())) for x
-             in module_dict.values()])
+            [
+                EqInlineKeyboardButton(
+                    x.__mod_name__,
+                    callback_data="{}_module({},{})".format(
+                        prefix, chat, x.__mod_name__.lower()
+                    ),
+                )
+                for x in module_dict.values()
+            ]
+        )
 
-    pairs = [
-    modules[i * 3:(i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)
-    ]
+    pairs = [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)]
 
     round_num = len(modules) / 3
     calc = len(modules) - round(round_num)
     if calc == 1:
-        pairs.append((modules[-1], ))
+        pairs.append((modules[-1],))
     elif calc == 2:
-        pairs.append((modules[-1], ))
-
-    max_num_pages = ceil(len(pairs) / 7)
-    modulo_page = page_n % max_num_pages
-
-    # can only have a certain amount of buttons side by side
-    if len(pairs) > 7:
-        pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("BACK", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("HOME", callback_data="bot_start"),
-             EqInlineKeyboardButton("NEXT", callback_data="{}_next({})".format(prefix, modulo_page)))]
-
-    else:
-        pairs += [[EqInlineKeyboardButton("HOME", callback_data="bot_start")]]
-
+        pairs.append((modules[-1],))
 
     return pairs
 
 
-def send_to_list(bot: Bot, send_to: list, message: str, markdown=False, html=False) -> None:
+def send_to_list(
+    bot: Bot, send_to: list, message: str, markdown=False, html=False
+) -> None:
     if html and markdown:
         raise Exception("Can only send with either markdown or HTML!")
     for user_id in set(send_to):
@@ -115,12 +115,15 @@ def revert_buttons(buttons):
 
     return res
 
-def sendMessage(text: str, bot: Bot, update: Update):
-    return bot.send_message(update.message.chat_id,
-                                    reply_to_message_id=update.message.message_id,
-                                    text=text, parse_mode=ParseMode.HTML)
-
-
 
 def is_module_loaded(name):
-     return name not in NO_LOAD
+    return name not in NO_LOAD
+
+
+def sendMessage(text: str, bot: Bot, update: Update):
+    return bot.send_message(
+        update.message.chat_id,
+        reply_to_message_id=update.message.message_id,
+        text=text,
+        parse_mode=ParseMode.HTML,
+    )
