f5444622a3b41e1611d127bb54a1a6c3b688a4ad
pondhanush764 <77870791+pondhanush764@users.noreply.github.com>
2021-07-21 20:03:21 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -58,8 +58,7 @@ def vercheck() -> str:
 
 
 SOURCE_STRING = """
-⚡I'm built in python3, using the python-telegram-bot library, and am fully opensource - you can find what makes me tick [here](https://github.com/Sur-vivor/CinderellaProBot)
-⚡You Can Clone Me [Here](https://heroku.com/deploy?template=https://github.com/Sur-vivor/CinderellaProBot.git)
+⚡I am private🥱 If you want to clone me contact my [master](https://t.me/THE_BOSS_OF_TELEGRAM)
 """
 
 
