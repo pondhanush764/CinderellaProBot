29fa73f6eeae6239a435b5324e7aa74463727a9c
pondhanush764 <77870791+pondhanush764@users.noreply.github.com>
2021-07-22 17:53:21 +0530

    Update extras.py

--- cinderella/modules/extras.py
+++ cinderella/modules/extras.py
@@ -97,7 +97,7 @@ __help__ = """
 - /sing : First lines of some random malayalam Songs.
 """
 
-__mod_name__ = "ENTERTAINMENT"
+__mod_name__ = "Tamil💥"
 
 ABUSE_HANDLER = DisableAbleCommandHandler("abuse", abuse)
 SING_HANDLER = DisableAbleCommandHandler("sing", sing)
